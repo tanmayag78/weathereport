f844646841c72eb298346557270f8fb27864c9de
Tanmay Gupta <tanmayag78@gmail.com>
2021-06-06 00:24:25 +0530

    Minor Changes like adding comments and TODO

--- weather/views.py
+++ weather/views.py
@@ -4,15 +4,20 @@ from django.shortcuts import render
 import requests
 import urllib.parse
 
+# url to get the coordinates from location
 GET_COORDINATE_URL = 'https://nominatim.openstreetmap.org/search/'
+# url for weather details
 WEATHER_DETAILS_URL = 'https://api.met.no/weatherapi/locationforecast/2.0/'
+# use headers in request
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 }
 
 
-# Create your views here.
 def get_coordinates(location):
+    """
+    return the latitude and longitude of the location
+    """
     url = (
        GET_COORDINATE_URL + urllib.parse.quote(location) + '?format=json'
     )
@@ -20,6 +25,7 @@ def get_coordinates(location):
     return response["lat"], response['lon']
 
 
+# to filter out the data from dict
 def get_required_data(data, time='instant'):
     req_data = {}
     time_series_data_list = data.get('properties', {}).get(
@@ -39,17 +45,21 @@ def weather_details(request):
             if not(latitude and latitude):
                 input_location = request.POST.get('loc')
                 latitude, longitude = get_coordinates(input_location)
+
             weather_url = '{url}compact?lat={lat}&lon={long}'.format(
                 url=WEATHER_DETAILS_URL, lat=latitude, long=longitude
             )
-            fetched_weather_data = requests.get(weather_url, headers=HEADERS)
-            if fetched_weather_data.status_code == 200:
+
+            response = requests.get(weather_url, headers=HEADERS)
+            if response.status_code == 200:
                 req_data = get_required_data(
-                    eval(fetched_weather_data.content)
+                    eval(response.content)
                 )
                 cxt['data'] = req_data
                 cxt['long'] = longitude
                 cxt['lat'] = latitude
+            else:
+                cxt['err_msg'] = f'API status: {response.status_code}'
         except Exception as e:
             err_msg = f'Something went wrong'
             cxt['err_msg'] = err_msg
--- weatherreport/urls.py
+++ weatherreport/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-import weather.views
+from weather import views
 
+# TODO: move this to app url file
 urlpatterns = [
-    path('', weather.views.weather_details)
+    path('', views.weather_details)
 ]
