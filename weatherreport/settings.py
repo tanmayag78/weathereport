07931280f45b077baf26cdab53df8e4b74f9e785
Tanmay Gupta <tanmayag78@gmail.com>
2021-06-06 02:43:15 +0530

    Getting Secret key from env

--- weatherreport/settings.py
+++ weatherreport/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,7 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(0_=^91dq9@wlt0p)9_(y#&p_c3w923x*ylc@^$+9r%x*mwldt'
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    default='django-insecure-(0_=^91dq9@wlt0p)9_(y#&p_c3w923x*ylc@^$+9r%x*mwldt'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
